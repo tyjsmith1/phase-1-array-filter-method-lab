965842c90775a16c5a55b66ee38bb0e8d9faabb6
tylersmith <tyjsmith1@gmail.com>
2023-11-01 16:42:55 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching (drivers, name) {
+    const matchingDrivers = drivers.filter(driver => {
+        const driverName = driver.toLowerCase();
+        const inputName = name.toLowerCase();
+
+        return driverName.includes(inputName);
+    })
+    return matchingDrivers;
+};
+
+function fuzzyMatch (drivers, name) {
+    const matchingLetters = drivers.filter(driver => {
+        const driverName = driver.toLowerCase();
+        const nameLower = name.toLowerCase();
+
+        return driverName.startsWith(nameLower);
+    });
+    return matchingLetters;
+}
+
+
+function matchName(drivers, query) {
+    const matchingDrivers = drivers.filter(driver => {
+      return driver.name.toLowerCase() === query.toLowerCase();
+    });
+  
+    return matchingDrivers;
+  }
+  
